5be702b2a442df75d577dfc0992ed20e89173ac7
deepthi <dpuducod@asu.edu>
2018-03-19 21:38:46 -0700

    BitMapFile skeleton

--- javaminibase/src/bitmap/BitMapFile.java
+++ javaminibase/src/bitmap/BitMapFile.java
@@ -3,18 +3,54 @@ package bitmap;
 import java.lang.*;
 import columnar.ValueClass;
 import columnar.*;
+import global.PageId;
+import global.SystemDefs;
+import heap.HFPage;
 
 public class BitMapFile
-{
-    public BitMapFile(String filename)
     {
+    private PageId headerPageId;
+    public Columnarfile columnfile;
+    public int ColumnNo;
+    public ValueClass value;
+    public String fileName;
+    private BitMapHeaderPage headerPage;
+    private String dbname;
 
-    }
+    public BitMapFile(String filename)
+            {
+                headerPageId = get_file_entry(filename);
+                headerPage = new BitMapHeaderPage(headerPageId);
+                dbname = new String(filename);
+            }
 
     public BitMapFile(String filename, Columnarfile columnfile,
-                      int ColumnNo, ValueClass value)
-    {
+            int ColumnNo, ValueClass value)
+            {
+                headerPageId = get_file_entry(filename);
+                if(headerPageId == null)
+                {
+                    this.columnfile = columnfile;    // create in Header page
+                    this.ColumnNo = ColumnNo;
+                    this.value = value;
+                    this.fileName = filename;
+                }
+                else
+                {
+                    // Yet to decide
+                }
+            }
 
+    private PageId get_file_entry(String filename)
+           // throws GetFileEntryException
+    {
+        try {
+            return SystemDefs.JavabaseDB.get_file_entry(filename);
+        } catch (Exception e) {
+            e.printStackTrace();
+           // throw new GetFileEntryException(e, "");
+        }
+        return null;
     }
 
     void close()
@@ -23,6 +59,11 @@ public class BitMapFile
     }
 
     void destroyBitMapFile()
+    {
+
+    }
+
+    void createBitMapFile(BitMapHeaderPage headerPage, HFPage heapPage)
     {
 
     }
@@ -46,4 +87,4 @@ public class BitMapFile
     {
 
     }
-}
+    }
